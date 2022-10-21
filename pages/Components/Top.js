d08faca7797249813d15e24eae42d4695fe5b9de
unknown <pixeljohann@gmail.com>
2022-10-21 14:55:54 -0400

    update

--- pages/Components/Top.js
+++ pages/Components/Top.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Nav from './Nav'
 export default function Top() {
     return (
-        <div className="bg-red">
+        <div className="">
             <Head>
                 <meta charset="UTF-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
